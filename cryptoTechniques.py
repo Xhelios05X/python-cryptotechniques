b2ee33961612ba6d5c21bef77f108b1ee58f2c8e
BartłomiejGazduslki <heluios05@gmail.com>
2024-06-07 14:57:55 +0200

    in cryptoTechniques.py help funcion was added and main function was edited

--- cryptoTechniques.py
+++ cryptoTechniques.py
@@ -1,9 +1,31 @@
 #!/bin/python3
 
 from Crypto.Protocol.KDF import PBKDF2
-
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+import argparse
+import sys
+
+# ToDo
+# - add description
+# - add argument 1
+# - add argument 2
+
+
+def Help():
+    parser = argparse.ArgumentParser(
+        prog = "CurrencyExchangeRate.py",
+        description = "ToDo",
+    )
+
+    parser.add_argument("--aes", action = "store_true")
+    parser.add_argument("filename")
+    parser.add_argument("password")
+    # Todo: add arguments
+
+    args = parser.parse_args()
+    return args
+
 
 def encriptionAES(stringToEncode:str, password:str):
     salt = "b'\x13\x85Q[W\xc8\x12Y \x81\xec\xb0%)\x15\xb5'"
@@ -21,5 +43,20 @@ def encriptionAES(stringToEncode:str, password:str):
         f.write(cipher.iv)
         f.write(ciphered_data)
 
+
+
+# main function
+# sys.argv[1] - filename
+# sys.argv[2] - password
 if __name__ == "__main__":
-    pass
+    help = Help()
+
+    filename = sys.argv[1]
+    password = sys.argv[2]
+
+    if help.aes:
+        with open(filename, "r") as f:
+            stringToEncode = f.read()
+        
+        encriptionAES(stringToEncode, password)
+    
